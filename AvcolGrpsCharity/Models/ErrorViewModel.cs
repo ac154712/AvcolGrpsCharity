d83c540ade1864239e96fa088aefabbd65c0d483
Ronald Arzadon <ac154712@avcol.school.nz>
2024-03-28 14:09:12 +1300

    Add project files.

---
+++ AvcolGrpsCharity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AvcolGrpsCharity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

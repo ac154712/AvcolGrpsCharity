4defa264292dc7ac9bc1bf72a046a419b37c7dbc
Ronald Arzadon <ac154712@avcol.school.nz>
2024-04-11 14:17:04 +1200

    fixed a SqlException: Invalid column name 'ChartyGrp_Name' Bug, just renamed a column in the sql database

--- AvcolGrpsCharity/Models/SignedCharityGrps.cs
+++ AvcolGrpsCharity/Models/SignedCharityGrps.cs
@@ -4,24 +4,24 @@ namespace AvcolGrpsCharity.Models
 {
     public class SignedCharityGrps
     {
-        [Key]
+        [Key] //setting the primary key
         public int CharityGrpID { get; set; }
 
-        [Required]
+        [Required] // making it so that it cant be null
         public string ChartyGrp_Name { get; set; }
 
         [Required]
         public string CharityGrp_description { get; set; }
 
         [Required]
-        [EmailAddress]
+        [EmailAddress] //making sure it is of the right format, specifically email address
         public string CharityGrp_email { get; set; }
 
         [Required]
-        [Phone]
+        [Phone] // using the phone format for this data field
         public string CharityGrp_phone { get; set; }
 
-        public ICollection<Donors> DonorsId { get; set; }
+        public ICollection<Donors> DonorsId { get; set; } // colecting DonorsId from Donors table
         public ICollection<CharityGrpStaff> CharityGrpStaffId { get; set; }
 
     }

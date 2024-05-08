e1243f97420146fa1696e0280e69989bad1e8fcd
Ronald Arzadon <ac154712@avcol.school.nz>
2024-05-08 15:14:44 +1200

    fixed the indexes to show the charity group name, instead of the id or description. also started adding the db initializers so i can input test data

---
+++ AvcolGrpsCharity/Data/MainContext.cs
@@ -0,0 +1,18 @@
+﻿using AvcolGrpsCharity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvcolGrpsCharity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<SignedCharityGrps> SignedCharityGrps { get; set; }
+        public DbSet<CharityGrpStaff> CharityGrpStaff { get; set; }
+        public DbSet<Donors> Donors { get; set; }
+        public DbSet<Donations> Donations { get; set; }
+        public DbSet<CharityCategory> CharityCategory { get; set; }
+    }
+}
